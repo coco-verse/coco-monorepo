13d23a4d7d2611efe76bfd806afe17259586ccdd
Janmajaya Mall <janmajaya.choithram@gmail.com>
2022-08-30 10:26:09 +0530

    fixes listening for chain events

--- packages/backend/src/chain_processor.js
+++ packages/backend/src/chain_processor.js
@@ -1,162 +1,101 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-import log from 'loglevel';
-log.setLevel(process.env.LOG_LEVEL ? process.env.LOG_LEVEL : 'trace');
-import { createAlchemyWeb3 } from '@alch/alchemy-web3';
-import { addresses, getMarketDetails, getMarketState } from './contracts';
-import { findSubmission, updateSubmissionDetails, addUserStake, increaseDonEscalationCount } from './db_manager';
-import { flairSubmissionWithOutcomeNo, removeSubmissionFlair } from './helpers';
-import { ethers } from 'ethers';
+import log from "loglevel";
+log.setLevel(process.env.LOG_LEVEL ? process.env.LOG_LEVEL : "trace");
+import {
+	getGroupContractInstance,
+	getMarketDetails,
+	getMarketState,
+} from "./contracts";
+import { findSubmission, updateSubmissionDetails } from "./db_manager";
+import { flairSubmissionWithOutcomeNo, removeSubmissionFlair } from "./helpers";
+import { ethers } from "ethers";
 
-const web3 = createAlchemyWeb3(process.env.ALCHEMY_WSS_URL);
+async function reflectOnChainUpdatesInDb(groupAddress, marketIdentifier) {
+	if (
+		!ethers.utils.isAddress(groupAddress) ||
+		!ethers.utils.isHexString(marketIdentifier)
+	) {
+		log.error(
+			`[reflectOnChainUpdatesInDb] received either invalid groupAddress:${groupAddress} or marketIdentifier:${JSON.stringify(
+				marketIdentifier
+			)}`
+		);
+		return;
+	}
+	// Status should be changed to removed if `outcome` is 0 and final
+	// (i.e.
+	// 	(donBufferEndsAt <= currentTime && resolutionBufferEndsAt == 0) ||
+	// 	(resolutionBufferEndsAt <= currentTime && resolutionBufferEndsAt != 0)
+	// )
+	// let currentTime = new Date().toISOString();
+	// if (
+	// 	// set status to REMOVED if final outcome == 0
+	// 	// Note that final outcome is considered declared when
+	// 	// (1) Buffer period expires && resolution period didn't
+	// 	// even start (2) Resolution period expired (3) Moderator
+	// 	// the outcome. In the third case, we intentionally expire
+	// 	// resolution period on-chain to indicate the market is
+	// 	// resolved. Thus, checking for (2) suffices for (3)
+	// 	// as well.
+	// 	((updates.donBufferEndsAt <= currentTime &&
+	// 		updates.resolutionBufferEndsAt == 0) ||
+	// 		(updates.resolutionBufferEndsAt <= currentTime &&
+	// 			updates.resolutionBufferEndsAt != 0)) &&
+	// 	updates.outcome == "0"
+	// ) {
+	// 	updates.status = constants.SUBMISSION_STATUS.REMOVED;
+	// }
 
-const eventSignatures = {
-  Challenged: '0x3acea3967c5be24f16b75421c5e477dd8b549db84bdfe6f359ec3eb9f1749ffb',
-  MarketCreated: '0xbc2bccc2713ac25dc4bcda6e2a6c18b5c5cd08d9c00fa807a841a510d6c11a79',
-  Redeemed: '0xa3364817cc6a3d1dfc5e4970c5ff96bbba39cb4182e88c06bad5ca2feffb1dca',
-  // TODO set correct value for `OutcomeSet`
-  OutcomeSet: '0xa3364817cc6a3d1dfc5e4970c5ff96bbba39cb4182e88c06bad5ca2feffb1dcT',
-};
+	log.info(
+		`[reflectOnChainUpdatesInDb] received on-chain update for marketIdentifier:${marketIdentifier} from groupAddress:${groupAddress}`
+	);
 
-const abiCoder = ethers.utils.defaultAbiCoder;
+	// Update market details in db
+	const marketState = await getMarketState(groupAddress, marketIdentifier);
+	const marketDetails = await getMarketDetails(
+		groupAddress,
+		marketIdentifier
+	);
 
-export async function eventsProcessor(error, logValue) {
-  if (error != undefined) {
-    log.error(`[eventsProcessor] log errored with ${error}`);
-    return;
-  }
+	await updateSubmissionDetails(marketIdentifier, {
+		...marketState,
+		...marketDetails,
+	});
 
-  const eventIdentifier = logValue.topics[0];
-  if (eventIdentifier == undefined) {
-    log.error(`[eventProcessor] Event received with missing event identifier`);
-    return;
-  }
-
-  log.info(`[eventProcessor] received event: ${JSON.stringify(logValue)}; LALALLAL`);
-
-  let marketIdentifier;
-  let groupAddress;
-  if (eventIdentifier == eventSignatures.Challenged) {
-    marketIdentifier = logValue.topics[1];
-    groupAddress = logValue.address;
-
-    // decode data
-    const data = abiCoder.decode(['address', 'uint256', 'uint8'], logValue.data);
-
-    // update user stake
-    addUserStake(data[0], groupAddress, marketIdentifier, 0, data[1].toString(), data[2]);
-
-    // update donEscalation Count
-    await increaseDonEscalationCount(marketIdentifier, 1);
-
-    log.info(
-      `[eventProcessor] "Challenged" event received; groupAddress=${groupAddress} marketIdentifier=${marketIdentifier}`
-    );
-  } else if (eventIdentifier == eventSignatures.MarketCreated) {
-    marketIdentifier = logValue.topics[1];
-    groupAddress = logValue.address;
-
-    // decode data
-    const data = abiCoder.decode(['address', 'address'], logValue.data);
-
-    // update stake for `creator`
-    addUserStake(
-      data[0],
-      groupAddress,
-      marketIdentifier,
-      // since this corresponds to user's initial challenge
-      // escalation index should be 1
-      1,
-      // initial challenge amount is always 0.5
-      '500000000000000000',
-      1
-    );
-
-    // update stake for `challenger`
-    addUserStake(data[1], groupAddress, marketIdentifier, 2, '1000000000000000000', 0);
-
-    // set donEscalationCount to 2
-    await increaseDonEscalationCount(marketIdentifier, 2);
-
-    log.info(
-      `[eventProcessor] "MarketCreated" event received; groupAddress=${groupAddress} marketIdentifier=${marketIdentifier}`
-    );
-  } else if (eventIdentifier == eventSignatures.Redeemed) {
-    marketIdentifier = logValue.topics[1];
-    groupAddress = logValue.address;
-    log.info(
-      `[eventProcessor] "Redeemed" event received; groupAddress=${groupAddress} marketIdentifier=${marketIdentifier}`
-    );
-  } else if (eventIdentifier == eventSignatures.OutcomeSet) {
-    marketIdentifier = logValue.topics[1];
-    groupAddress = logValue.address;
-    log.info(
-      `[eventProcessor] "OutcomeSet" event received; groupAddress=${groupAddress} marketIdentifier=${marketIdentifier}`
-    );
-  } else {
-    // TODO Updating submission state in mongo when
-    // final outcome is declared using setOutcome
-    // by the moderator is missing. Implement it.
-
-    log.info(`[eventProcessor] "UnknownEvent" event received; logValue=${JSON.stringify(logValue)}`);
-    return;
-  }
-
-  // Update market details in db
-  const marketState = await getMarketState(groupAddress, marketIdentifier);
-  const marketDetails = await getMarketDetails(groupAddress, marketIdentifier);
-
-  await reflectOnChainUpdatesInDb(marketIdentifier, {
-    ...marketState,
-    ...marketDetails,
-  });
-
-  const submission = await findSubmission(marketIdentifier);
-  if (submission != undefined) {
-    if (submission.outcome == '0') {
-      // Update the flair on submission to NO
-      flairSubmissionWithOutcomeNo(submission.submissionRedditId);
-    } else {
-      removeSubmissionFlair(submission.submissionRedditId);
-    }
-  }
+	const submission = await findSubmission(marketIdentifier);
+	if (submission != undefined) {
+		if (submission.outcome == "0") {
+			// Update the flair on submission to NO
+			flairSubmissionWithOutcomeNo(submission.submissionRedditId);
+		} else {
+			removeSubmissionFlair(submission.submissionRedditId);
+		}
+	}
 }
 
-async function reflectOnChainUpdatesInDb(marketIdentifier, updates) {
-  // Status should be changed to removed if `outcome` is 0 and final
-  // (i.e.
-  // 	(donBufferEndsAt <= currentTime && resolutionBufferEndsAt == 0) ||
-  // 	(resolutionBufferEndsAt <= currentTime && resolutionBufferEndsAt != 0)
-  // )
-  // let currentTime = new Date().toISOString();
-  // if (
-  // 	// set status to REMOVED if final outcome == 0
-  // 	// Note that final outcome is considered declared when
-  // 	// (1) Buffer period expires && resolution period didn't
-  // 	// even start (2) Resolution period expired (3) Moderator
-  // 	// the outcome. In the third case, we intentionally expire
-  // 	// resolution period on-chain to indicate the market is
-  // 	// resolved. Thus, checking for (2) suffices for (3)
-  // 	// as well.
-  // 	((updates.donBufferEndsAt <= currentTime &&
-  // 		updates.resolutionBufferEndsAt == 0) ||
-  // 		(updates.resolutionBufferEndsAt <= currentTime &&
-  // 			updates.resolutionBufferEndsAt != 0)) &&
-  // 	updates.outcome == "0"
-  // ) {
-  // 	updates.status = constants.SUBMISSION_STATUS.REMOVED;
-  // }
-  await updateSubmissionDetails(marketIdentifier, {
-    ...updates,
-  });
+export function subscribeGroupEvents(groupAddress) {
+	const groupContract = getGroupContractInstance(groupAddress);
+	groupContract.on(
+		"MarketCreated",
+		(marketIdentifier, creator, challenger, event) => {
+			reflectOnChainUpdatesInDb(groupAddress, marketIdentifier);
+		}
+	);
+	groupContract.on(
+		"Challenged",
+		(marketIdentifier, by, amount, outcome, event) => {
+			reflectOnChainUpdatesInDb(groupAddress, marketIdentifier);
+		}
+	);
+	groupContract.on("Redeemed", (marketIdentifier, by, event) => {
+		reflectOnChainUpdatesInDb(groupAddress, marketIdentifier);
+	});
+	groupContract.on("OutcomeSet", (marketIdentifier, event) => {
+		reflectOnChainUpdatesInDb(groupAddress, marketIdentifier);
+	});
 }
 
 export function startEventsSubscription() {
-  web3.eth.subscribe(
-    'logs',
-    {
-      address: addresses.Group,
-    },
-    eventsProcessor
-  );
+	subscribeGroupEvents(process.env.GROUP);
 }
